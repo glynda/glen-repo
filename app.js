215188125b6344415ab0dc90af2d24ef842101bf
glynda <127177056+glynda@users.noreply.github.com>
2023-03-12 23:58:28 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const  log = require('./logger');
+
+log('This is logging message');
+
---
+++ logger.js
@@ -0,0 +1,13 @@
+
+var myUrl = "https://mylogger.com";
+
+console.log(__filename);
+console.log(__dirname);
+
+function log(message){
+    console.log(message);
+}
+
+module.exports = log;
+
+
